ee9f38c032d5a8908a25d003d7aaf8e81adf8fb1
Aitor Suso <aitor.suso@socialpoint.es>
2024-03-30 12:33:49 +0100

    PR fixes

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,9 +1,7 @@
 import React, {forwardRef, useEffect, useImperativeHandle} from 'react';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-// import OSM from 'ol/source/OSM';
 import 'ol/ol.css';
-//import {BingMaps, OGCMapTile} from "ol/source";
 import {OGCMapTile} from "ol/source";
 import {fromLonLat} from 'ol/proj.js';
 import * as olExtent from 'ol/extent';
@@ -16,7 +14,7 @@ interface MapComponentProps {
 
 let currentCity: City = {} as City;
 
-export const MapComponent = forwardRef((props: MapComponentProps, ref) => {
+export const MapComponent = forwardRef(({onChangeScore}: MapComponentProps, ref) => {
 
     useImperativeHandle(ref, () => {
         return {
@@ -29,25 +27,12 @@ export const MapComponent = forwardRef((props: MapComponentProps, ref) => {
     }
 
     useEffect(() => {
-        // const defaultLayer = new TileLayer({
-        //     preload: Infinity,
-        //     source: new BingMaps({
-        //         key: 'AhUtDOf6zdSsMInl8K998jVZ-4fQkE7lELsHl-LOC1QOa-REIsx7hu4vk2c9jXCC',
-        //         imagerySet: 'Aerial'
-        //     }),
-        // })
-
         const rasterLayer = new TileLayer({
             source: new OGCMapTile({
                 url: 'https://maps.gnosis.earth/ogcapi/collections/NaturalEarth:raster:HYP_HR_SR_OB_DR/map/tiles/WebMercatorQuad',
                 crossOrigin: '',
             }),
         })
-
-        // const osmLayer = new TileLayer({
-        //     preload: Infinity,
-        //     source: new OSM(),
-        // })
 
         const defaultCoordinates = [-3.902496927433447, 39.859293765063946];
 
@@ -62,7 +47,6 @@ export const MapComponent = forwardRef((props: MapComponentProps, ref) => {
             controls: [],
         });
 
-        // const cityCoordinates = fromLonLat(props.city.coords);
         const leftTopCord = fromLonLat([-14.171880997799288, 44.49539323310686]);
         const rightBottomCord  = fromLonLat([4.820812930361028, 34.902218267988]);
         const extent = olExtent.boundingExtent([leftTopCord, rightBottomCord]);
@@ -76,13 +60,13 @@ export const MapComponent = forwardRef((props: MapComponentProps, ref) => {
             let length = lineString.getLength();
             let distanceInKms = Math.trunc(Math.round((length / 1000) * 100) / 100);
 
-            props.onChangeScore(distanceInKms);
+            onChangeScore(distanceInKms);
         })
 
         return () => {
             map.setTarget()
         };
-    }, []);
+    });
 
 
     return (
